21b6379ca190cfaee8754776815ba2f99b9e667e
Min Khant Tin <minkhanttin@gmail.com>
2026-09-21 14:56:55 +0700

    Add Docker deployment configuration for backend

--- my-next-backend-02/next.config.mjs
+++ my-next-backend-02/next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
-  reactCompiler: true,
+  output: "standalone",
+  basePath: "/backend",
 };
 
 export default nextConfig;
